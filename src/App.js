e332108592bc44db8490003217ccf9ba7f756c21
Cameron.Viehmann <Cameron.Viehmann@daugherty.com>
2022-01-15 13:47:49 -0600

    Lets goooo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from 'react';
+import { FormGroup, Checkbox, FormControlLabel } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddTask extends React.Component{
+  // addNewTask(){
+
+  // }
+
+  render(){
+   return(
+     <div>
+      <input type="text" id="newTask" />
+      <button onClick={i => this.props.onClick()}>submit</button>
+     </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tasks: ["Gaming"]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Header</h1>
+        </header>
+
+        <body>
+          <div>
+
+            <FormGroup>
+              {this.state.tasks.map(item => (
+                <FormControlLabel key={item} control={<Checkbox />} label={item} />
+                // <li key={item}>{item}</li>
+              ))}
+            </FormGroup>
+
+            <AddTask />
+          
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
